438ef2f8057d17a6b672c45b4c8fbf4dc483e5a1
Fajkowsky <dawidfajkowski@gmail.com>
2015-08-16 22:11:19 +0200

    adding and showing players

--- js/controllers/teamCtrl.js
+++ js/controllers/teamCtrl.js
@@ -2,4 +2,12 @@
 
 angular.module('app').controller('TeamCtrl', function($scope, TeamService) {
     $scope.team = TeamService.getName();
+    $scope.players = [];
+    $scope.newTeamPlayer = '';
+    $scope.playerNumber = $scope.players.length;
+
+    $scope.addNewPlayer = function () {
+        $scope.players.push($scope.newTeamPlayer);
+        $scope.playerNumber = $scope.players.length;
+    }
 });
