7a0257a4bf1f3a0ecd85468b7cafb00632209d4a
Fajkowsky <dawidfajkowski@gmail.com>
2015-08-24 21:21:24 +0200

    small refactoring

--- js/directives/uniqueInput.js
+++ js/directives/uniqueInput.js
@@ -3,10 +3,12 @@
 
     angular.module('app').directive('unique', function () {
         return {
-            scope: {players: '=unique'},
+            scope: {
+                players: '=unique'
+            },
             require: 'ngModel',
             link: function (scope, elem, attrs, ctrl) {
-                ctrl.$validators.unique = function(modelValue) {
+                ctrl.$validators.unique = function (modelValue) {
                     return scope.players.indexOf(modelValue) === -1;
                 };
             }
