1c9f34ed7c873c6647fe1e1130df6f9bf12457b2
Fajkowsky <dawidfajkowski@gmail.com>
2015-08-25 21:24:08 +0200

    fixed message about to few players

--- js/controllers/teamCtrl.js
+++ js/controllers/teamCtrl.js
@@ -4,6 +4,7 @@
     angular.module('app').controller('TeamCtrl', function ($scope, $location, TeamService) {
         var teamCtrl = $scope.teamCtrl = {
             team: TeamService.getName(),
+            minialPlayersAmount: 2,
             players: [],
             newTeamPlayer: ''
         };
